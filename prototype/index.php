d34226ab11db279d242db0628f0216877b994749
Mikayla Lisiy <mol25@drexel.edu>
2020-07-29 18:43:04 -0400

    Victoria's changes

--- prototype/index.php
+++ prototype/index.php
@@ -1,7 +1,3 @@
-<?php
-    require 'config.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,66 +10,15 @@
 </head>
 <body>
     <div class="logo">
-        <img src="images/prime_logo.svg" class="logoimg">
-    </div>
-    <!-- top nav -->
-    <ul>
-        <li><a href="#">For You</a></li>
-        <li><a href="#">Originals</a></li>
-        <li><a href="#">Movies</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">Prime</a></li>
-    </ul>
-    <!-- featured content -->
-    <br></br>
-    <!-- Swiper -->
-    <div class="swiper-container">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <img src="images/thevastofnight_poster.jpg">
-            </div>
-            <div class="swiper-slide">
-                <img src="images/paprika_poster.jpg">
-            </div>
-            <div class="swiper-slide">
-                <img src="images/trailofufos_poster.jpg">
-            </div>
-            <div class="swiper-slide">
-                <img src="images/aceofhearts_poster.jpg">
-            </div>
-            <div class="swiper-slide">
-                <img src="images/astarisborn_poster.jpg">
-            </div>
-        </div>
-        <!-- Add Pagination -->
-        <div class="swiper-pagination"></div>
-    </div>
-    <h3>Continue Watching</h3>
-        
-    <!-- bottom navigation -->
-    <div class="navbar">
-        <a href="index.html"><img src="images/icons/home.svg" class="svg"></a>
-        <a href="#"><img src="images/icons/store.svg" class="svg"></a>
-        <a href="#"><img src="images/icons/search.svg" class="svg"></a>
-        <a href="#"><img src="images/icons/downloads.svg" class="svg"></a>
-        <a href="#"><img src="images/icons/mystuff.svg" class="svg"></a>
+            <img src="images/prime_logo.svg" class="logoimg">
+            <h4>Choose Profile</h4>
     </div>
 
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <div id="options">
+        <div class="profile_btn"> <a href="home.html" class="profile_button"> Example Name</a> </div>
+        <div class="child_btn"> <a href="home.html" class="child_button"> Child</a> </div>
+        <div class="add_btn"> <a href="addname.php" id="add_profile"> + Add New Profile</a> </div>
+    </div>
 
-     <!-- Initialize Swiper -->
-    <script>
-        var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 'auto',
-        spaceBetween: 40,
-        centeredSlides: true,
-        grabCursor: true,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        });
-    </script>
 </body>
 </html>
