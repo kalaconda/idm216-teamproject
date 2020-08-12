b1ff4c7a90ea0b7a984d3323827a8dae43141387
Mikayla Lisiy <mol25@drexel.edu>
2020-08-12 10:18:58 -0400

    add age.php temp

---
+++ prototype/age.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Prime Video</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://use.fontawesome.com/cce24d2a9e.js"></script>
+</head>
+<body>
+    <!-- top bar -->
+    <div class="logo">
+            <img src="images/prime_logo.svg" class="logoimg">
+            <h4>Age</h4>
+    </div>
+    <div id="add_name">
+        <h5>What is your age range?</h5> <br>
+        
+    </div>
+
+    <!-- bottom navigation -->
+    <div class="skipanddone">
+        <div class="skip">
+            <a href="home.php"><h3>BACK</h3></a>
+        </div>
+        <div class="done">
+            <a href="home.php"><h3>NEXT</h3></a>
+        </div>
+    </div>
+
+</body>
+</html>
