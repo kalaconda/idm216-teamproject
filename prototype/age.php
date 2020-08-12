ee1a18e9d04572cecbb3e385828c71e66de0f468
Dolma Sherpa <dolma2000@gmail.com>
2020-08-13 00:25:43 +0900

    finished the styling for Age.php
    In home.php, made it so the home icon goes to index.php, not index.html.
    
    Addname.php - fixed the next and back button to the right page.

--- prototype/age.php
+++ prototype/age.php
@@ -16,13 +16,44 @@
     </div>
     <div id="add_name">
         <h5>What is your age range?</h5> <br>
-        
+
+        <div class="age_range">
+
+            <button type="button">
+                <p><span class="bullet_dots"></span>Below 13</p>
+            </button>
+
+            <button type="button">
+                <p><span class="bullet_dots"></span>Between 13 and 17</p>
+            </button>
+
+            <button type="button">
+                <p><span class="bullet_dots"></span>Above 18</p>
+            </button>
+
+        </div>
+      
     </div>
+
+    <!-- script to select multiple buttons -->
+    <script>
+        $("button").click(function () {
+      clicked = true;
+      if (clicked) {
+        $(this).toggleClass('active');
+        clicked = true;
+      } else {
+        $(this).removeClass('active');
+        clicked = false;
+      }
+    });
+    </script>
+
 
     <!-- bottom navigation -->
     <div class="skipanddone">
         <div class="skip">
-            <a href="home.php"><h3>BACK</h3></a>
+            <a href="addname.php"><h3>BACK</h3></a>
         </div>
         <div class="done">
             <a href="home.php"><h3>NEXT</h3></a>
