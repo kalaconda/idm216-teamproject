7c13f2cc5a854a58317dd2c599029b90295af4e4
Mikayla Lisiy <mol25@drexel.edu>
2020-08-21 22:08:12 -0400

    Revert "Revert "💄css based on changes""
    This reverts commit 732ce83285f212700c23d15388ffc60679fd7e1d.

--- prototype/home.php
+++ prototype/home.php
@@ -20,6 +20,7 @@
     <title>Prime Video</title>
     <script src="https://use.fontawesome.com/20cbea2300.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="../package/swiper-bundle.min.css">
     <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>"> <!-- force css to work -->
 
 </head>
@@ -37,10 +38,10 @@
     <!-- top nav -->
     <ul>
         <li><a href="home.php" class="homelink">For You</a></li>
+        <li><a href="#">Prime</a></li>
+        <li><a href="#">TV</a></li>
         <li><a href="#">Originals</a></li>
         <li><a href="#">Movies</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">Prime</a></li>
     </ul>
 
     <!-- featured content -->
@@ -103,7 +104,7 @@
 
     <!-- categories -->
     <div class="item">
-        <h3 class="categorytt">Prime Picks</h3>
+        <h3 class="categorytt">Prime Picks For You</h3>
         <div class="thumbnail poster_tn">
             <div>
                 <img src="images/paprika_poster.jpg" alt="paprika">
@@ -211,7 +212,7 @@
 
     <!-- actors -->
     <div class="item">
-        <h3 class="categorytt">Actors/Actresses</h3>
+        <h3 class="categorytt">Recommended Actors</h3>
         <div class="thumbnail2">
             <div class="inlineflex">
                 <img src="images/actors/annehathaway.jpg" alt="annehathaway">
@@ -230,8 +231,7 @@
     
     <!-- bottom navigation -->
     <div class="navbar">
-        <a href="index.php"><img src="images/icons/home_blue.svg" class="svg"></a>
-        <a href="#"><img src="images/icons/store.svg" class="svg"></a>
+        <a href="home.php"><img src="images/icons/home_blue.svg" class="svg"></a>
         <a href="#"><img src="images/icons/search.svg" class="svg"></a>
         <a href="#"><img src="images/icons/downloads.svg" class="svg"></a>
         <a href="index.php"><img src="images/icons/myprofile.svg" class="svg"></a>
