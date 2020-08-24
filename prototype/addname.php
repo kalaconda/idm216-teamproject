fd9072ea14fc82e81078348fda95e179f9b68bbb
Mikayla Lisiy <mol25@drexel.edu>
2020-08-24 15:22:01 -0400

    👾 name input microinteraction

--- prototype/addname.php
+++ prototype/addname.php
@@ -7,6 +7,10 @@
     <title>Prime Video</title>
     <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>"> <!-- force css to work -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <!-- input script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.0/additional-methods.min.js"></script>
 </head>
 <body>
 
@@ -22,7 +26,11 @@
     </div>
     <div id="add_name">
         <h5>What is your name?</h5> <br>
-        <input type="text" placeholder="Enter your name" id="name_input">
+        <form>
+            <span class="notvalid"><input type="text" placeholder="Enter your name..." 
+            name="enteryourname" pattern="^[a-zA-Z]{1,20}$" title="Must Include Characters Only" required>
+            &nbsp;</span>
+        </form>
     </div>
 
     <!-- bottom navigation -->
@@ -37,6 +45,21 @@
     <div class="next">
             <a href="age.php"><h3>NEXT</h3></a>
     </div>
+
+    <!-- input script -->
+    <script>
+    $('[name=enteryourname]').keyup(function(){
+
+        if($('form').valid()){
+            $(this).parent().removeClass().addClass('isvalid'
+    );
+        }else{
+                $(this).parent().removeClass().addClass('notvalid');
+            
+        }
+
+    })
+    </script>
 
 </body>
 </html>
